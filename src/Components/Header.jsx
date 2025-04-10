53d1dea03176b136a62659e169309ad23753691b
Mustafa <mostafamohamed12434@gmail.com>
2025-04-10 16:31:52 +0200

    .

---
+++ src/Components/Header.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { RiShoppingCartFill } from "react-icons/ri";
+function Header() {
+  return (
+    <div>
+      <div className="navbar bg-C4 text-primary-content">
+        <div className="flex-1">
+          <button className="btn btn-ghost hover:bg-sky-500 hover:border-none border-none hover:shadow-none shadow-none text-xl">
+            Hardware Store
+          </button>
+        </div>
+        <div className="flex items-center gap-5">
+          <button className="btn btn-ghost hover:bg-sky-500 hover:border-none border-none hover:shadow-none shadow-none transition-all duration-200 rounded-full py-7 ">
+            <RiShoppingCartFill className="text-2xl " />
+          </button>
+          <div className="avatar">
+            <div className="w-16 rounded-full">
+              <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 import daisyui from "daisyui";
 export default {
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-	theme: {
-		extend: {},
-	},
-	plugins: [daisyui],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        C1: "#1031E3",
+        C2: "#725CE3",
+        C3: "#BD10E3",
+        C4: "#1073E3",
+        C5: "#3310E3",
+      },
+    },
+  },
+  plugins: [daisyui],
 };
